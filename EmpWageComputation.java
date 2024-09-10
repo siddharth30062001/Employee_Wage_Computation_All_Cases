1993838852e977a4ee48d7260c3fd3db825fef7c
siddharth30062001 <siddharthrajput263@gmail.com>
2024-09-10 22:06:35 +0530

    Employee attendence

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,7 +2,15 @@ package Employee_Wage_Computation_All_Cases;
 
 public class EmpWageComputation {
     public static void main(String[] args) {
-
         System.out.println("Welcome to Employee Wage Computation Program");
-}
+
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+
+        if (empCheck == IS_FULL_TIME) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is absent");
+        }
+    }
 }
